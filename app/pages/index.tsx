c85b61055c933cb268d274908881ae69b68d8593
Aidan Sharp <mhofutheweirdo@gmail.com>
2022-08-23 18:16:12 -0400

    homepage card refactoring

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -7,37 +7,37 @@ const Home: BlitzPage = () => {
 	return (
 		<main className={styles.main}>
 			<CluckHUD theme={chud.Moon} />
+
 			<div className={styles.cont}>
-				<div className={styles.grid}>
-					<div className={styles.card}>
-						<h2>About Me</h2>
+				<div className={styles.bio}>
+					<h2>About Me</h2>
 
-						<p>
-							Hey, Sia here. Nice place, huh?
-							<br />
-							It&apos;s taken quite a few redesigns, and this certainly won&apos;t be
-							the last :D
-							<br />
-							I&apos;m just a computer science student with a little too much free
-							time.
-							<br />
-							Why don&apos;t you check out the place while you&apos;re here?
-							<br />
-							You can also take a look at my{" "}
-							<a href="https://siarune.me/" target="_blank" rel="noopener noreferrer">
-								old website
-							</a>
-							, or where it all started on{" "}
-							<a
-								href="https://github.com/Siarune"
-								target="_blank"
-								rel="noopener noreferrer"
-							>
-								Github
-							</a>
-						</p>
-					</div>
+					<p>
+						Hey, Sia here. Nice place, huh?
+						<br />
+						It&apos;s taken quite a few redesigns, and this certainly won&apos;t be the
+						last :D
+						<br />
+						I&apos;m just a computer science student with a little too much free time.
+						<br />
+						Why don&apos;t you check out the place while you&apos;re here?
+						<br />
+						You can also take a look at my{" "}
+						<a href="https://siarune.me/" target="_blank" rel="noopener noreferrer">
+							old website
+						</a>
+						, or where it all started on{" "}
+						<a
+							href="https://github.com/Siarune"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							Github
+						</a>
+					</p>
+				</div>
 
+				<div className={styles.feed}>
 					<div className={styles.card}>
 						<h2>
 							<Link href="/projects/sauce">
@@ -64,8 +64,23 @@ const Home: BlitzPage = () => {
 						</h2>
 						<h4>He hates everything and it&apos;s great</h4>
 					</div>
+
+					<div className={styles.card}>
+						<h2>
+							<Link href="/apps">
+								<a>Apps</a>
+							</Link>
+						</h2>
+						<h4>A curated list of quality-of-life android apps</h4>
+					</div>
 				</div>
+				{/* <div className={styles.containr}>
+						<div className={styles.chevron}></div>
+						<div className={styles.chevron}></div>
+						<div className={styles.chevron}></div>
+					</div> */}
 			</div>
+
 			<footer className={styles.footer}>
 				<p>
 					Created and maintained by Aidan Sharp and open source on Github under the GPLv3
