57515272b17993d8521592c9d269f92872aed636
siarune <aidan.sharp@siarune.dev>
2023-11-13 14:37:35 -0500

    add markdown renderer

--- src/routes/blogish/post/[name].tsx
+++ src/routes/blogish/post/[name].tsx
@@ -1,47 +1,46 @@
-import {RouteDataFuncArgs, useRouteData} from "solid-start"
-import {For, Suspense} from "solid-js"
+import { RouteDataFuncArgs, useRouteData } from "solid-start"
+import { For, Suspense } from "solid-js"
 import db from "~/db"
-import {post} from "~/db/schema"
-import {eq} from "drizzle-orm"
+import { post } from "~/db/schema"
+import { eq } from "drizzle-orm"
 import { createRouteData } from "solid-start"
-
+import { SolidMarkdown } from "solid-markdown"
 
 export function routeData({ params }: RouteDataFuncArgs) {
-
 	const query = params.name.replaceAll("%20", " ")
 
-	return createRouteData(async () => {
-		return db
-			.select({
-				name: post.name,
-				content: post.content
-			})
-			.from(post)
-			.where(eq(post.name, query))
-
-	}, {
-		key: () => query
-	})
+	return createRouteData(
+		async () => {
+			return db
+				.select({
+					name: post.name,
+					content: post.content,
+				})
+				.from(post)
+				.where(eq(post.name, query))
+		},
+		{
+			key: () => query,
+		}
+	)
 }
 
 export default function Post() {
-
 	const Post = useRouteData<typeof routeData>()
 
 	return (
 		<main>
 			<Suspense fallback={<></>}>
 				<For each={Post()}>
-					{(posts) => <div>
-						<h2>{posts.name}</h2>
+					{(posts) => (
+						<div>
+							<h2>{posts.name}</h2>
 
-						<p>
-							{posts.content}
-						</p>
-					</div>}
+							<SolidMarkdown children={posts.content} />
+						</div>
+					)}
 				</For>
 			</Suspense>
 		</main>
 	)
 }
-
