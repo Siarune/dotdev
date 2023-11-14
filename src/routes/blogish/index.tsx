6d3129c09e06636811ab15e559a75ee45054f414
siarune <aidan.sharp@siarune.dev>
2023-11-14 10:55:53 -0500

    scaffolding

--- src/routes/blogish/index.tsx
+++ src/routes/blogish/index.tsx
@@ -7,32 +7,35 @@ import {desc, eq} from "drizzle-orm"
 // Yes, the export is necessary
 export function routeData() {
 	return createServerData$(() =>
-		db.select({
+		db
+			.select({
 				id: post.id,
-				name: post.name
+				name: post.name,
 			})
 			.from(post)
+			.where(eq(post.isPublic, true))
 			.orderBy(desc(post.id))
 			.limit(500)
 	)
 }
 
 export default function Blogish() {
-
-	const Posts = useRouteData<typeof routeData>()
+	const Post = useRouteData<typeof routeData>()
 
 	return (
 		<main class={"main"}>
 			<Suspense>
-				<>
-					<For each={Posts()}>
-						{(post) =>
-							<A href={`./post/${post.name}`}>
-								{post.name}
+				<ul class="w50vw h-full mt20vh p0 list-none text-center">
+					<For each={Post()}>
+						{(post) => (
+							<A class="decoration-none m0" href={`./post/${post.name}`}>
+								<li class="p8 text-xl text-center hover:bg-stone animate-duration-100 transition-500">
+									{ post.name }
+								</li>
 							</A>
-						}
+						)}
 					</For>
-				</>
+				</ul>
 			</Suspense>
 		</main>
 	)
---
+++ src/routes/blogish/studio/edit.tsx
@@ -0,0 +1,5 @@
+export default function StudioEdit() {
+	return(
+		<main></main>
+	)
+}
