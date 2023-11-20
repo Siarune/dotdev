cc711453bcfafdcabc063fd54905650fa2df1ba5
siarune <aidan.sharp@siarune.dev>
2023-11-20 11:32:34 -0500

    vercel adapter

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,19 @@
 import solid from "solid-start/vite"
-import { defineConfig } from "vite"
+import {defineConfig} from "vite"
 import vercel from "solid-start-vercel"
 import UnoCSS from "unocss/vite"
 
-export default defineConfig({
-	plugins: [
-		solid({
-			ssr: true,
-			adapter: vercel({
-				//@ts-ignore
-				prerender: true
-			})
-		}),
-		UnoCSS()
-	]
+export default defineConfig(() => {
+
+	return {
+		plugins: [
+			solid({
+				ssr: true,
+				adapter: vercel({
+					prerender: { expiration: 5*60 }
+				})
+			}),
+			UnoCSS()
+		],
+	}
 })
