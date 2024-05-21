511dfa782f0eb751e205e9faa62a1a075dc8af44
siarune <aidan.sharp@siarune.dev>
2024-05-21 16:03:41 -0400

    solid devtools

--- app.config.ts
+++ app.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
+import devtoolsPlugin from "solid-devtools/vite";
 
 export default defineConfig({
 	ssr: true,
@@ -7,6 +8,11 @@ export default defineConfig({
 		preset: "vercel"
 	},
 	vite: {
-		plugins: [UnoCSS()]
+		plugins: [
+			UnoCSS(),
+			devtoolsPlugin({
+				autoname: true
+			})
+		]
 	}
 });
--- src/entry-client.tsx
+++ src/entry-client.tsx
@@ -1,4 +1,5 @@
 import { mount, StartClient } from "@solidjs/start/client";
+import 'solid-devtools'
 
 // @ts-ignore
 mount(() => <StartClient/>, document.getElementById("app"));
