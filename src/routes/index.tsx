d6c4860fef53c4ae53d1b6c983dd2a43b9585b97
siarune <asharp@siarune.dev>
2026-01-13 20:57:00 -0500

    update homepage

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,22 +3,33 @@ import { A } from "@solidjs/router"
 export default function Home() {
 	return (
 		<main class="main justify-center">
-			<title>Siarune.dev</title>
-			<div class="color-txt text-xl md:max-w-40vw p4">
-				<p>
+			<title>Siarune's place</title>
+			<div class="color-txt text-xl md:max-w-50vw p4">
+				<p class="mb">
 					Hello, and welcome to my website!
 					<br />
-					My name's Aidan Sharp, but I usually go by <em>siarune</em>.
+					My name's Aidan Sharp, but I usually go by <em>
+						siarune
+					</em>{" "}
+					online.
 					<br /> <br />
-					There's not much to look at for now, but you can explore at
-					your leisure.
+					There's not much to look at for now, but I'm working on it.
 				</p>
-				<A
-					class="decoration-underline decoration-dashed color-txt"
-					href="/blogish"
-				>
-					[Blog-ish]
-				</A>
+				<div class="flex-row decoration-underline decoration-dashed">
+					<A
+						href="https://github.com/Siarune"
+						class="m-1"
+					>
+						[github]
+					</A>
+					<A
+						href="https://resume.rune.nz"
+						target="_blank"
+						class="m-1"
+					>
+						[resume]
+					</A>
+				</div>
 			</div>
 		</main>
 	)
